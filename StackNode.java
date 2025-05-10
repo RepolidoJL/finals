9e31bf8428ac74319ef6ef9ac2f2c039c8b079a9
RepolidoJL <149493582+RepolidoJL@users.noreply.github.com>
2025-05-10 14:43:21 +0800

    Create StackNode.java

---
+++ StackNode.java
@@ -0,0 +1,60 @@
+public class StackNode {
+   private Object[] items;
+   private int count;
+
+ 
+   public StackArray(int size) {
+      if (size > 50 || size < 5) {
+         size = 10;
+      }
+      items = new Object[size];
+      count = 0;
+   }
+
+
+   public boolean isFull() {
+      return count == items.length;
+   }
+
+ 
+   public boolean isEmpty() {
+      return count == 0;
+   }
+
+  
+   public boolean push(Object value) {
+      if (!isFull()) {
+         items[count] = value;
+         count++;
+         return true;
+      }
+      return false;
+   }
+
+  
+   public Object peek() {
+      if (!isEmpty()) {
+         return items[count - 1]; 
+      }
+      return null;
+   }
+
+  
+   public boolean pop() {
+      if (!isEmpty()) {
+         items[count - 1] = null; 
+         count--; 
+         return true;
+      }
+      return false;
+   }
+
+
+   public String toString() {
+      String result = "";
+      for (int i = count - 1; i >= 0; i--) {
+         result = result + "[" + items[i] + "]\n";
+      }
+      return result;
+   }
+}
